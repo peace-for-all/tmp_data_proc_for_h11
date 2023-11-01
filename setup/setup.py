1737bf0172e787e9d7817354b2507c3af20d3702
Valia Walsk <valjaer@gmail.com>
2023-11-01 19:05:40 +0300

    adds draft for setup

---
+++ setup/setup.py
@@ -0,0 +1,20 @@
+# todo Create venv, install requirements, install mysql on host if doesn't exist
+# todo maybe create a user who will rule them all
+
+
+
+
+
+
+
+
+
+
+
+# For SQL
+# mysql> CREATE USER 'scripts'@'localhost' IDENTIFIED BY 'PASSWORD';
+# mysql> CREATE DATABASE skills;
+# mysql> GRANT ALL PRIVILEGES ON skills.* TO 'scripts'@'localhost';
+# mysql> FLUSH PRIVILEGES;
+# todo then put these settings in local .env file!
+
